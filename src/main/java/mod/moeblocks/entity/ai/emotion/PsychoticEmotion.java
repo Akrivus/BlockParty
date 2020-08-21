ce8dd89b8b1863d25de65f5c6fa5a12fadda257d
Owen Adrian <akrivus@gmail.com>
2020-08-21 09:31:04 -0700

    Yanderes kill others under stress.

--- src/main/java/mod/moeblocks/entity/ai/emotion/PsychoticEmotion.java
+++ src/main/java/mod/moeblocks/entity/ai/emotion/PsychoticEmotion.java
@@ -1,15 +1,13 @@
 package mod.moeblocks.entity.ai.emotion;
 
 import mod.moeblocks.entity.StateEntity;
-import mod.moeblocks.entity.ai.behavior.BeehiveBehavior;
 import mod.moeblocks.entity.ai.goal.RevengeGoal;
 import mod.moeblocks.entity.util.Emotions;
 import mod.moeblocks.entity.util.VoiceLines;
 import mod.moeblocks.entity.util.data.Relationships;
 import mod.moeblocks.util.DistanceCheck;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.monster.IMob;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.SoundEvent;
 
 import java.util.List;
@@ -17,6 +15,22 @@ import java.util.stream.Collectors;
 
 public class PsychoticEmotion extends AbstractEmotion {
     protected MurderGoal murderGoal;
+
+    @Override
+    public void tick() {
+        this.entity.world.addParticle(ParticleTypes.EFFECT, this.entity.getCenteredRandomPosX(), this.entity.getCenteredRandomPosY(), this.entity.getPosZRandom(this.entity.getWidth() / 2.0F), 0.0F, this.entity.getGaussian(0.2D), 0.0F);
+        super.tick();
+    }
+
+    @Override
+    public SoundEvent getLivingSound() {
+        return VoiceLines.EMOTION_PSYCHOTIC.get(this.entity);
+    }
+
+    @Override
+    public Emotions getKey() {
+        return Emotions.PSYCHOTIC;
+    }
 
     @Override
     public void start() {
@@ -34,16 +48,6 @@ public class PsychoticEmotion extends AbstractEmotion {
     }
 
     @Override
-    public SoundEvent getLivingSound() {
-        return VoiceLines.EMOTION_PSYCHOTIC.get(this.entity);
-    }
-
-    @Override
-    public Emotions getKey() {
-        return Emotions.PSYCHOTIC;
-    }
-
-    @Override
     public boolean canAttack(LivingEntity target) {
         Relationships relationships = this.entity.getRelationships();
         return !(relationships.get(target).canDieFor() || relationships.isFavorite(target));
@@ -57,8 +61,7 @@ public class PsychoticEmotion extends AbstractEmotion {
 
         @Override
         public boolean preCheckTarget() {
-            List<LivingEntity> victims = this.entity.world.getEntitiesWithinAABB(LivingEntity.class, this.entity.getBoundingBox().grow(8.0F, 4.0F, 8.0F)).stream().filter(victim -> this.entity.canAttack(victim) && this.entity.isSuperiorTo(victim)).collect(Collectors.toList());
-            victims.sort(new DistanceCheck(this.entity));
+            List<LivingEntity> victims = this.entity.world.getEntitiesWithinAABB(LivingEntity.class, this.entity.getBoundingBox().grow(8.0F, 4.0F, 8.0F)).stream().filter(victim -> this.entity.canAttack(victim) && this.entity.isSuperiorTo(victim)).sorted(new DistanceCheck(this.entity)).collect(Collectors.toList());
             this.victim = victims.isEmpty() ? null : victims.get(0);
             return true;
         }
