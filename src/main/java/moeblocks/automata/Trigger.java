d344093f25f5686084a55420671dbd19039889bc
Owen Adrian <akrivus@gmail.com>
2020-12-29 07:45:39 -0800

    Added trigger class.

---
+++ src/main/java/moeblocks/automata/Trigger.java
@@ -0,0 +1,15 @@
+package moeblocks.automata;
+
+import moeblocks.entity.AbstractNPCEntity;
+
+import java.util.function.Predicate;
+
+public class Trigger<E extends AbstractNPCEntity, T extends IStateEnum<E>> {
+    private final T state;
+    private final Predicate<E> function;
+    
+    public Trigger(T state, Predicate<E> function) {
+        this.state = state;
+        this.function = function;
+    }
+}
