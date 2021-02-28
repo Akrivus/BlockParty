0615d00c2234389703befa5c77c13bc8ee811506
Owen Adrian <akrivus@gmail.com>
2021-02-28 00:19:38 -0700

    Added callbacks for waypoints.

--- src/main/java/moeblocks/block/WaypointBlock.java
+++ src/main/java/moeblocks/block/WaypointBlock.java
@@ -3,6 +3,14 @@ package moeblocks.block;
 import moeblocks.automata.state.enums.RibbonColor;
 import moeblocks.automata.state.enums.TimeOfDay;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class WaypointBlock extends Block {
     protected final RibbonColor color;
@@ -16,5 +24,16 @@ public class WaypointBlock extends Block {
 
     public WaypointBlock(Properties properties, RibbonColor color) {
         this(properties, color, null);
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        super.onBlockPlacedBy(world, pos, state, placer, stack);
+
+    }
+
+    @Override
+    public void onBlockHarvested(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        super.onBlockHarvested(world, pos, state, player);
     }
 }
