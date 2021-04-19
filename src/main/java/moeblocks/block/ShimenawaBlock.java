bcc869d6e0410d730f0ef2c9dc5f07d9c9536bf4
Owen Adrian <akrivus@gmail.com>
2021-04-18 21:33:39 -0700

    Fixed shimenawa and torii tablet models.

--- src/main/java/moeblocks/block/ShimenawaBlock.java
+++ src/main/java/moeblocks/block/ShimenawaBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
@@ -12,26 +13,26 @@ import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class ShimenawaBlock extends Block {
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
-    protected static final VoxelShape NORTH_AABB = Block.makeCuboidShape(1.0D, 1.0D, 0.0D, 15.0D, 15.0D, 2.0D);
-    protected static final VoxelShape EAST_AABB = Block.makeCuboidShape(14.0D, 1.0D, 1.0D, 16.0D, 15.0D, 15.0D);
-    protected static final VoxelShape SOUTH_AABB = Block.makeCuboidShape(1.0D, 1.0D, 14.0D, 15.0D, 15.0D, 16.0D);
-    protected static final VoxelShape WEST_AABB = Block.makeCuboidShape(0.0D, 1.0D, 1.0D, 2.0D, 15.0D, 15.0D);
+    public static final BooleanProperty HANGING = BooleanProperty.create("hanging");
+    protected static final VoxelShape NORTH_AABB = Block.makeCuboidShape(0.0D, 12.0D, 12.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape EAST_AABB = Block.makeCuboidShape(0.0D, 12.0D, 0.0D, 4.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SOUTH_AABB = Block.makeCuboidShape(0.0D, 12.0D, 0.0D, 16.0D, 16.0D, 4.0D);
+    protected static final VoxelShape WEST_AABB = Block.makeCuboidShape(12.0D, 12.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 
     public ShimenawaBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH).with(HANGING, true));
     }
 
     @Override
     public void neighborChanged(BlockState state, World world, BlockPos pos, Block block, BlockPos neighbor, boolean isMoving) {
-        BlockPos behind = pos.offset(state.get(FACING));
-        if (world.isAirBlock(behind)) {
+        BlockPos base = pos.offset(state.get(HANGING) ? Direction.UP : state.get(FACING).getOpposite());
+        if (world.isAirBlock(base)) {
             world.destroyBlock(pos, true);
         }
     }
@@ -51,30 +52,25 @@ public class ShimenawaBlock extends Block {
         switch (state.get(FACING)) {
         default:
         case NORTH:
-            return NORTH_AABB;
+            return state.get(HANGING) ? NORTH_AABB.withOffset(0.0, 0.0, -0.375) : NORTH_AABB;
         case EAST:
-            return EAST_AABB;
+            return state.get(HANGING) ? EAST_AABB.withOffset(0.375, 0.0, 0.0) : EAST_AABB;
         case SOUTH:
-            return SOUTH_AABB;
+            return state.get(HANGING) ? SOUTH_AABB.withOffset(0.0, 0.0, 0.375) : SOUTH_AABB;
         case WEST:
-            return WEST_AABB;
+            return state.get(HANGING) ? WEST_AABB.withOffset(-0.375, 0.0, 0.0) : WEST_AABB;
         }
     }
 
     @Override
-    public VoxelShape getCollisionShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
-        return VoxelShapes.empty();
-    }
-
-    @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        if (context.getFace().getAxis().isVertical()) { return null; }
-        return this.getDefaultState().with(FACING, context.getFace().getOpposite());
+        BlockState state = this.getDefaultState().with(HANGING, context.getFace() == Direction.DOWN);
+        return state.with(FACING, context.getPlacementHorizontalFacing().getOpposite());
     }
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, HANGING);
     }
 }
 
--- src/main/java/moeblocks/block/ToriiTabletBlock.java
+++ src/main/java/moeblocks/block/ToriiTabletBlock.java
@@ -22,15 +22,29 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class ToriiTabletBlock extends Block implements ITileEntityProvider {
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    protected static final VoxelShape NORTH_AABB = Block.makeCuboidShape(2.0D, 0.0D, 10.0D, 14.0D, 14.0D, 16.0D);
+    protected static final VoxelShape EAST_AABB = Block.makeCuboidShape(0.0D, 0.0D, 2.0D, 6.0D, 14.0D, 14.0D);
+    protected static final VoxelShape SOUTH_AABB = Block.makeCuboidShape(2.0D, 0.0D, 0.0D, 14.0D, 14.0D, 6.0D);
+    protected static final VoxelShape WEST_AABB = Block.makeCuboidShape(10.0D, 0.0D, 2.0D, 16.0D, 14.0D, 14.0D);
 
     public ToriiTabletBlock(Properties properties) {
         super(properties);
         this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World world, BlockPos pos, Block block, BlockPos neighbor, boolean isMoving) {
+        BlockPos behind = pos.offset(state.get(FACING).getOpposite());
+        if (world.isAirBlock(behind)) {
+            world.destroyBlock(pos, true);
+        }
     }
 
     @Override
@@ -53,13 +67,33 @@ public class ToriiTabletBlock extends Block implements ITileEntityProvider {
         return state.rotate(mirror.toRotation(state.get(FACING)));
     }
 
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+        switch (state.get(FACING)) {
+        default:
+        case NORTH:
+            return NORTH_AABB;
+        case EAST:
+            return EAST_AABB;
+        case SOUTH:
+            return SOUTH_AABB;
+        case WEST:
+            return WEST_AABB;
+        }
+    }
+
     private BlockPattern getGatePattern() {
         return BlockPatternBuilder.start().aisle("#######", "###X###", " #   # ", " #   # ", " #   # ", " #   # ").where('#', CachedBlockInfo.hasState((state) -> state.isIn(MoeTags.Blocks.SAKURA_WOOD))).where('X', CachedBlockInfo.hasState(BlockStateMatcher.forBlock(MoeBlocks.TORII_TABLET.get()))).where(' ', CachedBlockInfo.hasState((state) -> !state.isSolid())).build();
     }
 
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+        Direction facing = context.getPlacementHorizontalFacing();
+        if (context.getWorld().getBlockState(context.getPos().offset(facing)).isIn(MoeTags.Blocks.SAKURA_WOOD)) {
+            return this.getDefaultState().with(FACING, facing.getOpposite());
+        } else {
+            return null;
+        }
     }
 
     @Override
