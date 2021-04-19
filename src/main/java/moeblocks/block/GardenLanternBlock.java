72f3aa84fe04eee8188f2b41fb091f533b791c02
Owen Adrian <akrivus@gmail.com>
2021-04-18 21:32:57 -0700

    Added LIT block property to garden lantern.

--- src/main/java/moeblocks/block/GardenLanternBlock.java
+++ src/main/java/moeblocks/block/GardenLanternBlock.java
@@ -1,31 +1,26 @@
 package moeblocks.block;
 
 import moeblocks.block.entity.GardenLanternTileEntity;
-import moeblocks.block.entity.ToriiTabletTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.RedstoneTorchBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class GardenLanternBlock extends Block implements ITileEntityProvider {
-    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
 
     public GardenLanternBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+        this.setDefaultState(this.getDefaultState().with(LIT, true));
     }
 
     @Override
@@ -38,27 +33,12 @@ public class GardenLanternBlock extends Block implements ITileEntityProvider {
     }
 
     @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        return state.with(FACING, rotation.rotate(state.get(FACING)));
-    }
-
-    @Override
-    public BlockState mirror(BlockState state, Mirror mirror) {
-        return state.rotate(mirror.toRotation(state.get(FACING)));
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
-    }
-
-    @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(LIT);
     }
 
     @Override
     public TileEntity createNewTileEntity(IBlockReader world) {
-        return new ToriiTabletTileEntity();
+        return new GardenLanternTileEntity();
     }
 }
