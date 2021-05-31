c65f4b944985865b80414ad7d0b58c7d235defc9
Owen Adrian <akrivus@gmail.com>
2021-05-30 19:57:19 -0700

    Fireflies spawn on tagged blocks.

--- src/main/java/moeblocks/init/MoeTags.java
+++ src/main/java/moeblocks/init/MoeTags.java
@@ -16,6 +16,7 @@ public class MoeTags {
     public static final ITag.INamedTag<Block> WINGED = BlockTags.createOptional(new ResourceLocation(MoeMod.ID, "moes/winged"));
 
     public static class Blocks {
+        public static final ITag.INamedTag<Block> FIREFLY_BLOCKS = BlockTags.createOptional(new ResourceLocation(MoeMod.ID, "firefly_blocks"));
         public static final ITag.INamedTag<Block> MOEABLES = BlockTags.createOptional(new ResourceLocation(MoeMod.ID, "moeables"));
         public static final ITag.INamedTag<Block> SAKURA_LOGS = BlockTags.createOptional(new ResourceLocation(MoeMod.ID, "sakura_logs"));
         public static final ITag.INamedTag<Block> SAKURA_WOOD = BlockTags.createOptional(new ResourceLocation(MoeMod.ID, "sakura_wood"));
