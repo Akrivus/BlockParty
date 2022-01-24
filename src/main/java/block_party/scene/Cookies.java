074c2a6ae1608ae7d169b0ea60ee9c65568adb7e
Owen Adrian <akrivus@gmail.com>
2022-01-23 21:58:10 -0700

    Cookies are now multidimensional.

--- src/main/java/block_party/scene/Cookies.java
+++ src/main/java/block_party/scene/Cookies.java
@@ -1,25 +1,32 @@
 package block_party.scene;
 
 import block_party.utils.NBT;
+import com.google.common.collect.Maps;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.StringTag;
-import org.apache.commons.compress.utils.Lists;
 
-import java.util.List;
+import java.util.Map;
 
 public class Cookies {
-    private final List<String> cookies = Lists.newArrayList();
+    private final Map<String, String> cookies = Maps.newHashMap();
 
     public Cookies() { }
 
     public Cookies(CompoundTag compound) {
-        compound.getList("Cookies", NBT.STRING).forEach((cookie) -> this.cookies.add(cookie.getAsString()));
+        compound.getList("Cookies", NBT.COMPOUND).forEach((element) -> {
+            CompoundTag counter = (CompoundTag) element;
+            String key = counter.getString("Key");
+            this.cookies.put(key, counter.getString("Value"));
+        });
     }
 
     public CompoundTag save(CompoundTag compound) {
         ListTag list = new ListTag();
-        this.cookies.forEach((cookie) -> list.add(StringTag.valueOf(cookie)));
+        this.cookies.forEach((key, value) -> {
+            CompoundTag element = new CompoundTag();
+            element.putString("Key", key);
+            element.putString("Value", value);
+        });
         compound.put("Cookies", list);
         return compound;
     }
@@ -29,14 +36,22 @@ public class Cookies {
     }
 
     public boolean has(String cookie) {
-        return this.cookies.contains(cookie);
+        return this.cookies.containsKey(cookie);
+    }
+
+    public String get(String cookie) {
+        return this.cookies.get(cookie);
+    }
+
+    public void add(String cookie, String value) {
+        this.cookies.put(cookie, value);
     }
 
     public void add(String cookie) {
-        this.cookies.add(cookie);
+        this.add(cookie, "");
     }
 
-    public void eat(String cookie) {
+    public void delete(String cookie) {
         this.cookies.remove(cookie);
     }
 }
