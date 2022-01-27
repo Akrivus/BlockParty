02999e930a68517a928c44a1f10599e088194390
Owen Adrian <akrivus@gmail.com>
2022-01-26 22:29:39 -0700

    Added senpai class.

---
+++ src/main/java/block_party/entities/Senpai.java
@@ -0,0 +1,10 @@
+package block_party.entities;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class Senpai extends BlockPartyNPC {
+    public Senpai(EntityType<Senpai> type, Level level) {
+        super(type, level);
+    }
+}
