1c4415d98b4595b936df4ac3c4907a6fe7b6a727
Owen Adrian <akrivus@gmail.com>
2022-01-29 23:18:43 -0700

    Added chunk despawn to cell phone.

--- src/main/java/block_party/messages/CNPCTeleport.java
+++ src/main/java/block_party/messages/CNPCTeleport.java
@@ -2,6 +2,7 @@ package block_party.messages;
 
 import block_party.entities.BlockPartyNPC;
 import block_party.world.CellPhone;
+import block_party.world.chunk.ForcedChunk;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
@@ -20,5 +21,6 @@ public class CNPCTeleport extends CNPCQuery {
         CellPhone cellPhone = new CellPhone(this.npc, player);
         BlockPartyNPC npc = cellPhone.call();
         if (npc != null) { npc.teleport(player.getLevel(), cellPhone); }
+        ForcedChunk.get(this.id).despawn();
     }
 }
